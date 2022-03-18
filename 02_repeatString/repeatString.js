394e258530422a38b4dc660fd6d13145aeb8cf79
nanimouse <0120codes@gmail.com>
2022-03-17 23:51:54 -0400

    got thru the first three!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = (string, num) => {
 
+    if (num < 0){
+        return "ERROR";
+    }
+    
+    let words = ''; //empty string to have place for return
+
+    for (i=0; i < num; i++){ 
+        words += string; // words = words + string
+
+    }
+
+    return words;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = string => {
+    let str = '';
+    //use split method to separate characters and return new array
+    let splitString = string.split("");
+
+    //use reverse method for reversal
+    let reverseIt = splitString.reverse();
+
+    //use join to rejoin the characters
+    let bringItBack = reverseIt.join("");
+
+    return bringItBack;
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+// npm test reverseString.spec.js
